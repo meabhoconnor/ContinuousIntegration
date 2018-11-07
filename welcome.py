c4159a8254f2add5197b4a2d7bf33cb16ac3b737
Meabh OConnor <meabh.oconnor@cit.ie>
2018-11-07 17:38:26 +0000

    Formatted date string output

--- welcome.py
+++ welcome.py
@@ -7,6 +7,7 @@
 import datetime
 now = datetime.datetime.now()
 print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))	
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
 #print("Hello world!")
+	
